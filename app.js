9e84b990270bc826f32fe2affc2aae752d55b6fe
John Graham <johngrhm@gmail.com>
2018-11-08 18:25:27 -0500

    Added sessions model and migrations Configured Sequelize to connect on start of application and migrate sessions model.

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express');
+require('./config/config');
+const models = require('./models');
 
 const app = express();
 
 app.get('/', (req, res) => { res.send('Hello World!') })
+
+models.sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+if (CONFIG.app === 'dev') {
+  models.sequelize.sync();
+}
 
 module.exports = app;
