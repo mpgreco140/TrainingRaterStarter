ecb85782efe0a32207e935add1d0fd2bb29c9efe
John Graham <johngrhm@gmail.com>
2018-11-10 17:09:44 -0500

    Fixed bug where no res was being returned.

--- controllers/SessionsController.js
+++ controllers/SessionsController.js
@@ -52,7 +52,7 @@ const update = async function (req, res) {
     return res.json({ success: false, error: err });
   }
 
-  return res;
+  return res.json(session);
 }
 module.exports.update = update;
 
