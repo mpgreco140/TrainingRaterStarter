e2a534f3b75ba2eed28106b76bfb6dd5b6d80259
ccamac <ccamac@milestechnologies.com>
2018-11-13 08:12:26 -0500

    get sessions from back end
    update the sessions service to get the sessions data from the backend
    API, used an interface for sessions to type them, and pulled sessions
    into their own module away from the app module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-import { SessionsService } from './sessions/sessions.service';
-import { SessionsListComponent } from './sessions/sessions-list/sessions-list.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SessionsModule } from './sessions/sessions.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SessionsListComponent,
     HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
+    SessionsModule,    
     AppRoutingModule,
     NgbModule
   ],
-  providers: [SessionsService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/sessions/sessions.service.ts
+++ src/app/sessions/sessions.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+export interface ISession {
+  id: number;
+  name: string;
+  location: string;
+  startTime: string;
+  createdAt: string;
+  updatedAt: string;
+}
 
 @Injectable()
 export class SessionsService {
-  sessionsMock = [{ Name: 'John Teaches Angular', Location: 'Miles-U 1' },
-  { Name: 'Scott Teaches AWS', Location: 'Miles-U 2' },
-  { Name: 'Jack Teaches PODIS', Location: 'Jacks Desk' },
-  ];
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
 
-  getSessions(): {}[] {
-    return this.sessionsMock;
+  getSessions(): Observable<ISession[]> {
+    return this.http.get<ISession[]>('http://localhost:3000/sessions');
   }
 }
