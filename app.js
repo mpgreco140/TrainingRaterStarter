a4daf52aa6f119aa96f802330237b8f92f67b760
mpg140 <grecom6@students.rowan.edu>
2018-11-19 17:45:48 -0500

    Updated app.js CORS config

--- app.js
+++ app.js
@@ -6,12 +6,19 @@ const sessions = require
 const app = express();
 
 // CORS
-app.use(function (req,res,next) {
-  //Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  //Pass to next layer of middleware
-  next();
-});
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization, Content-Type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+  });
 
 app.get('/', (req, res) => { res.send('Hello World!') })
 
@@ -21,7 +28,7 @@ models.sequelize
         console.log('Connection has been established successfully.');
     })
     .catch(err => {
-        console.error('Unable to connecr to the database:', err);
+        console.error('Unable to connect to the database:', err);
     });
 
 if (CONFIG.app == 'dev') {
