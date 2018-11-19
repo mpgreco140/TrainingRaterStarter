e3bbdb5d4449b187552adefc8892de73dca7a5d2
mpg140 <grecom6@students.rowan.edu>
2018-11-19 17:37:42 -0500

    Updated app.js

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const express = require('express');
+require ('./config/config');
+const models = require('./models');
+const sessions = require
 
 const app = express();
 
+// CORS
+app.use(function (req,res,next) {
+  //Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  //Pass to next layer of middleware
+  next();
+});
+
 app.get('/', (req, res) => { res.send('Hello World!') })
+
+models.sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connecr to the database:', err);
+    });
+
+if (CONFIG.app == 'dev') {
+    models.sequelize.sync();
+} 
+
+app.get('/sessions', sessions.getAll)
 
 module.exports = app;
