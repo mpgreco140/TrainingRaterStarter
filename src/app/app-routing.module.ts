55b616436ffc0d56d44720210b418f5cca47b961
mpg140 <grecom6@students.rowan.edu>
2018-12-06 18:02:11 -0500

    Updated project files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { HomeComponent } from './home/home.component';
 import { SessionsListComponent } from './sessions/sessions-list/sessions-list.component';
 import { NgModule } from '@angular/core';
 import { SessionsDetailComponent } from './sessions/session-detail/session-detail.component';
+import { AuthGuard } from './common/auth/auth.guard';
+import { LoginComponent } from './common/auth/login.component';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'sessions', component: SessionsListComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'sessions', component: SessionsListComponent, canActivate: [AuthGuard] },
   { path: 'sessions/:sessionId', component: SessionsDetailComponent },
 ];
 
