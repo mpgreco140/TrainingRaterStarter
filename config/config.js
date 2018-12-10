a54b9c39ab774d5b5646fbdf6c5c36cf0fd23638
mpg140 <grecom6@students.rowan.edu>
2018-12-10 17:50:00 -0500

    Updated DB information

--- config/config.js
+++ config/config.js
@@ -4,6 +4,6 @@ CONFIG.app = 'dev';
 CONFIG.db_dialect = 'mysql';
 CONFIG.db_name = 'TrainingRaterStarter';
 CONFIG.db_user = 'root';
-CONFIG.db_password = 'E@$yYe$T3rd@y';
+CONFIG.db_password = 'E@$yYe$T3rd@Y';
 CONFIG.jwt_encryption = 'aiwushfaeiuvnaeifunva02';
 CONFIG.jwt_expiration = '1000';
